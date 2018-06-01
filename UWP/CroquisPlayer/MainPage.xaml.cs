ab2407bf3fa1434a887cb8e388511908c13bede1
gaucho1218 <gaucho1218@gmail.com>
2018-06-01 21:52:14 +0900

    #11 WIP

--- UWP/CroquisPlayer/MainPage.xaml.cs
+++ UWP/CroquisPlayer/MainPage.xaml.cs
@@ -63,7 +63,7 @@ namespace CroquisPlayer
         {
             var picker = new Windows.Storage.Pickers.FileOpenPicker();
             picker.ViewMode = Windows.Storage.Pickers.PickerViewMode.Thumbnail;
-            picker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.Desktop;
+            picker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.PicturesLibrary;
             picker.FileTypeFilter.Add(".jpg");
             picker.FileTypeFilter.Add(".jpeg");
             picker.FileTypeFilter.Add(".png");
