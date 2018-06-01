82279ad4c7fbd7addadd20dfee79a05c9cf61596
gaucho1218 <gaucho1218@gmail.com>
2018-06-01 16:19:41 +0900

    #5 WIP changing for dynamic photos

--- UWP/CroquisPlayer/MainPage.xaml.cs
+++ UWP/CroquisPlayer/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -22,6 +23,8 @@ namespace CroquisPlayer
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private ObservableCollection<Button> dataCollection;
+
         public MainPage()
         {
             this.InitializeComponent();
